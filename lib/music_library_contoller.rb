17492138d5e3c75989a2046de2d50d495a808db2
elijahmm17 <elijahmm17@gmail.com>
2019-06-01 05:30:09 +0000

    Automatically backed up by Learn

--- lib/music_library_contoller.rb
+++ lib/music_library_contoller.rb
@@ -28,7 +28,7 @@ class MusicLibraryController
       when 'list genres'
         list_genres 
       when 'list artist'
-        list_songs_by_artists
+        list_songs_by_artist
       when 'list genre' 
         list_songs_by_genre 
       when 'play song' 
