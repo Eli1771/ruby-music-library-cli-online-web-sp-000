1d463b04f608673b082c1b5be6210a407265a8ab
elijahmm17 <elijahmm17@gmail.com>
2019-06-01 05:24:42 +0000

    Automatically backed up by Learn

--- lib/music_library_contoller.rb
+++ lib/music_library_contoller.rb
@@ -77,8 +77,6 @@ class MusicLibraryController
     puts "Which song number would you like to play?"
     input = gets.chomp
     song = Song.find_by_name(input)
-    if !song.nil?
-      puts "Playing #{song.name} by #{song.artist}"
-    end 
+    puts "Playing #{song.name} by #{song.artist}" if !song.nil?
   end 
 end 
